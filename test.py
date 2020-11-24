eeb73031c40982ab595dfe6568a07fcaccd2ee91
Edward Swalker <edwardtang0611@gmail.com>
2020-11-24 08:49:55 +0800

    add command prompt

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-import requests from requests.auth 
-import HTTPBasicAuth
-r=requests.post('http://mir.com/api/v2.0.0/missions',                 headers={"Authorization": "Basic %s" % 'YWRtaW46OGM2OTc2ZTViNTQxMDQxNWJkZTkwOGJkNGRlZTE1ZGZiMTY3YTljODczZmM0YmI4YTgxZjZmMmFiNDQ4YTkxOA=='})
+import requests
+from requests.auth import HTTPBasicAuth
+inputCommand = input('Enter your name:')
+print('Hello, ' + inputCommand)
+r=requests.post('http://mir.com/api/v2.0.0/missions',
+                headers={"Authorization": "Basic %s" % 'YWRtaW46OGM2OTc2ZTViNTQxMDQxNWJkZTkwOGJkNGRlZTE1ZGZiMTY3YTljODczZmM0YmI4YTgxZjZmMmFiNDQ4YTkxOA=='})
