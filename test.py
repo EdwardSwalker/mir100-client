b671b053ec677477e5570775229600deafeeb00d
Edward Swalker <edwardtang0611@gmail.com>
2020-11-25 16:39:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,9 @@ import requests
 from requests.auth import HTTPBasicAuth
 inputCommand = input('Enter your name:')
 print('Hello, ' + inputCommand)
-r=requests.post('http://mir.com/api/v2.0.0/missions',
-                headers={"Authorization": "Basic %s" % 'YWRtaW46OGM2OTc2ZTViNTQxMDQxNWJkZTkwOGJkNGRlZTE1ZGZiMTY3YTljODczZmM0YmI4YTgxZjZmMmFiNDQ4YTkxOA=='})
+
+if inputCommand == 'status':
+    r=requests.get('http://mir.com/api/v2.0.0/status',
+                    headers={"Authorization": "Basic %s" % 'YWRtaW46OGM2OTc2ZTViNTQxMDQxNWJkZTkwOGJkNGRlZTE1ZGZiMTY3YTljODczZmM0YmI4YTgxZjZmMmFiNDQ4YTkxOA=='})
+# r=requests.post('http://mir.com/api/v2.0.0/missions',
+# headers={"Authorization": "Basic %s" % 'YWRtaW46OGM2OTc2ZTViNTQxMDQxNWJkZTkwOGJkNGRlZTE1ZGZiMTY3YTljODczZmM0YmI4YTgxZjZmMmFiNDQ4YTkxOA=='})
