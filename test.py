9b3f1ec695ddf6059ec02c4599bb532a697a804d
Edward <edwardtang0611@gmail.com>
2020-11-26 21:02:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import json
 import requests
 from requests.auth import HTTPBasicAuth
-commandList = ['1 - Status', '2 - Mission List']
+commandList = ['1 - Status', '2 - Mission List', '3 - Add Mission to Queue']
 print(*commandList, sep='\n')
 inputCommand = input('Please enter command number:')
 print('Running' + ' ' + commandList[int(inputCommand) - 1] + '...' + '\n')
@@ -18,4 +18,20 @@ if inputCommand == '2':
     dictData = json.loads(r.text)
     for dictObject in dictData:
         print(dictObject['name'])
+
+if inputCommand == '3':
+    inputMissionName = input('Please enter the mission name that you wish to add the queue:')
+    r=requests.get('http://mir.com/api/v2.0.0/missions',
+                    headers={"Authorization": "Basic %s" % 'YWRtaW46OGM2OTc2ZTViNTQxMDQxNWJkZTkwOGJkNGRlZTE1ZGZiMTY3YTljODczZmM0YmI4YTgxZjZmMmFiNDQ4YTkxOA=='})                
+    dictData = json.loads(r.text)
+    for dictObject in dictData:
+        if inputMissionName == dictObject['name']:
+            missionGuid = dictObject['guid']
+            headers = {
+                "Content-Type": "json",
+                "Authorization": "Basic %s" % "YWRtaW46OGM2OTc2ZTViNTQxMDQxNWJkZTkwOGJkNGRlZTE1ZGZiMTY3YTljODczZmM0YmI4YTgxZjZmMmFiNDQ4YTkxOA=="
+            }
+            addNewMission=requests.post('http://mir.com/api/v2.0.0/mission_queue',
+                                         headers=headers)
+            print(addNewMission.text)
 
